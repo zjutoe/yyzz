335c6f5b60ed6f3a1e6af7ef581a200eec93e1bf
Toe Zju <zjutoe@gmail.com>
2020-12-08 20:57:33 +0800

    download douban wish/reading/finished lists
    Signed-off-by: Toe Zju <zjutoe@gmail.com>

--- book.py
+++ book.py
@@ -8,7 +8,6 @@ import requests
 import bs4
 import json
 
-books = []
 
 def req_get(url):
     return requests.get(url, headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safiri/537.36 SE 2.X MetaSr 1.0"})
@@ -66,36 +65,61 @@ def book_count(soup):
     count = re.findall('([0-9]+)', t)[-1]
     return int(count)
 
-        
-def book_wish_list_all(bookurl):
+
+
+def book_list_all(url):
     ret = []
 
     num_book = 0
-    r = req_get(bookurl)
+    r = req_get(url)
     if r.status_code == 200:
         soup = bs4.BeautifulSoup(r.text, 'html.parser')
         num_book = book_count(soup)
         ret += parse_book_list(soup)
 
     for page in range(15, num_book, 15):
-        time.sleep(3*random.random())
-        r = req_get(bookurl + ('?sort=time&start=%d&filter=all&mode=grid&tags_sort=count' % page))
+        time.sleep(1 + 2*random.random()) # sleep for a random period to avoid being banned
+        r = req_get(url + ('?sort=time&start=%d&filter=all&mode=grid&tags_sort=count' % page))
         if r.status_code == 200:
             soup = bs4.BeautifulSoup(r.text, 'html.parser')
             ret += parse_book_list(soup)
         
     return ret
+    
+
+# wish to read
+def book_wish_list_all(uid):
+    bookurl = 'https://book.douban.com/people/%s/wish' % uid
+    return book_list_all(bookurl)
 
 
-# print(books)
-# href
+# currently reading
+def book_reading_list_all(uid):
+    bookurl = 'https://book.douban.com/people/%s/do' % uid
+    return book_list_all(bookurl)
+
+
+# finished read 
+def book_finished_list_all(uid):
+    bookurl = 'https://book.douban.com/people/%s/collect' % uid
+    return book_list_all(bookurl)
+
+
 
 if __name__ == '__main__':
     id = sys.argv[1]
 
-    bwish = 'https://book.douban.com/people/%s/wish' % id
+    with open('read_%s_wish.json' % id) as f:
+        for b in book_wish_list_all(bwish):
+            f.writeline(b.tojson())
 
-    print(bwish)
-    for b in book_wish_list_all(bwish):
-        print(b.tojson())
+    with open('read_%s_reading.json' % id) as f:
+        for b in book_reading_list_all(bwish):
+            f.writeline(b.tojson())
+
+
+    with open('read_%s_finished.json' % id) as f:
+        for b in book_finished_list_all(bwish):
+            f.writeline(b.tojson())
+            
 
